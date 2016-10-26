3e4ee1a4ed4e30977a75e0447c5123e31d650ddc
Yu-Yang Lin <yyl289@nyu.edu>
2016-10-26 13:55:14 -0700

    Test

--- 1_TwoSum.java
+++ 1_TwoSum.java
@@ -2,6 +2,9 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
     	if (nums == null || nums.length < 0) {
     		return nums;
-    	}   
+    	}
+    	// The added message
+
+    	return new int[];
     }
 }
